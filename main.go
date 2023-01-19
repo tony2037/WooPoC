d8f1c25985fb5a76f987c38d36bee55a952e7c9a
ztex <ztex030640417@gmail.com>
2023-01-19 17:42:27 +0800

    Implement command submission API
    [Problem]
    Need an API to submit command
    
    [Solution]
    As impl
    
    [test]
    go run .
    https://127.0.0.1:3030/submit?command="ls ."

--- main.go
+++ main.go
@@ -13,12 +13,17 @@ func index(w http.ResponseWriter, r *http.Request) {
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	serverPort := 3030
+	certFile := "cert.pem"
+	keyFile := "key.pem"
 
-	pTLS := NewTLSGenerator("cert.pem", "key.pem")
+	pTLS := NewTLSGenerator(certFile, keyFile)
 	pTLS.GenKey()
+
+	pMaster := NewMaster()
 
 	// Reference: https://pkg.go.dev/net/http#ListenAndServeTLS
 	http.HandleFunc("/", index)
+	http.HandleFunc("/submit", pMaster.MasterTaskSubmission)
 	log.Printf("About to listen on %v. Go to https://127.0.0.1:%v/", serverPort, serverPort)
 	err := http.ListenAndServeTLS(fmt.Sprintf(":%v", serverPort), pTLS.certFile, pTLS.keyFile, nil)
 	log.Fatal(err)
